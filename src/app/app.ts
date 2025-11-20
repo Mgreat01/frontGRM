89bef1e010135f391c00b5dc7790875a541949c2
Mgreat01 <ephraimmonga5@gmail.com>
2025-11-20 11:13:35 +0100

    ajout de tailwind css et flowbite dans le projet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('gemedical-app');
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }

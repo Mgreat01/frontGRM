d3b83a0446b4ea6931e6d3125a037808a546b94d
Mgreat01 <ephraimmonga5@gmail.com>
2025-11-24 14:22:18 +0100

    refactor:  amelioration de la gestion du standalone et d utilisation de flowbite

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import 'zone.js';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()), // ✔️ ici, PAS dans App
+  ]
+});

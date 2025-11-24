6724944bf174380c123fde57cda778b2bbe446fd
Mgreat01 <ephraimmonga5@gmail.com>
2025-11-24 14:27:54 +0100

    feat et refactor : Implémentation de la logique et amelioration du composants d inscription

--- src/app/auth/rgst/rgst.ts
+++ src/app/auth/rgst/rgst.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-rgst',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule], 
   templateUrl: './rgst.html',
   styleUrl: './rgst.css',
 })
 export class Rgst {
+  noms_complet = '';
+  email = '';
+  motdepasse = '';
+  type = '';
+  specialiteId = '';
 
+  constructor(private auth: Auth, private router: Router) {}
+
+  register(): void {
+    const user: any = {
+      noms_complet: this.noms_complet,
+      email: this.email,
+      motdepasse: this.motdepasse,
+      type: this.type,
+      specialiteId: this.type === 'medecin' ? this.specialiteId : null
+    };
+
+    this.auth.registerUser(user).subscribe(() => {
+      console.log(user);
+      
+      console.log('User registered successfully');
+      this.router.navigate(['/login']);
+    });
+  }
+
+  onTypeChange(): void {
+    if (this.type !== 'medecin') {
+      this.specialiteId = '';
+    }
+  }
 }

c983696cd956b00be0bd5aa5ce001a40f1d2aa80
Mgreat01 <ephraimmonga5@gmail.com>
2025-11-24 14:23:25 +0100

    creation des nouvelles routes pour l app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Rgst } from './auth/rgst/rgst';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login,
+    },
+    {
+        path: 'rgst',
+        component: Rgst,
+    },
+    { path: '',
+      redirectTo: '/login',
+      pathMatch: 'full' },
+];

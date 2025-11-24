e992806709f08dcd12e1f1dc06eea96dd5c001bf
Mgreat01 <ephraimmonga5@gmail.com>
2025-11-24 14:29:17 +0100

    feat: implémentation de la fonctionnalité de connexion utilisateur

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { Router, RouterModule } from '@angular/router';
+import { Utilisateur } from '../../models/utilisateur';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  email = '';
+  motdepasse = '';
+  errorMessage = '';
+
+  constructor(private auth: Auth, private router: Router) {}
+
+  login(): void {
+    
+    const user: any = {
+      email: this.email,
+      motdepasse: this.motdepasse
+    };
+
+    this.auth.loginUser(user).subscribe({
+      next: (res: any) => {
+        if (res?.token) {
+          this.auth.setToken(res.token);
+          this.router.navigate(['/rgst']); // redirection
+        }
+      },
+      error: (err) => {
+        console.error('Erreur de connexion', err);
+        this.errorMessage = 'Email ou mot de passe incorrect';
+      }
+    });
+  }
 }

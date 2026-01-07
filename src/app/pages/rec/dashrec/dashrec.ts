e4992273b99a9ed9e4de7afb95adcdd861093847
Mgreat01 <ephraimmonga5@gmail.com>
2026-01-07 16:38:31 +0100

    feat : integration de la methode de recuperation des symptomes

--- src/app/pages/rec/dashrec/dashrec.ts
+++ src/app/pages/rec/dashrec/dashrec.ts
@@ -1,83 +1,124 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Auth } from '../../../services/auth';
 import { User } from '../../../services/user';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { forkJoin } from 'rxjs'; 
 
 @Component({
   selector: 'app-dashrec',
+  standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './dashrec.html',
   styleUrl: './dashrec.css',
 })
-export class Dashrec {
-nom_patient = '';
-email_patient = '';
-numero_patient = '';
-symptome = '';
-date_rdv = '';
-heure_rdv = '';
-specialiteId = '';
-medecinId = '';
+export class Dashrec implements OnInit {
+  form = {
+    nom_patient: '',
+    email_patient: '',
+    numero_patient: '',
+    date_rdv: '',
+    heure_rdv: '',
+    medecinId: ''
+  };
 
-disponibilites: any[] = [];
-rdvs: any[] = [];
-medecins: any[] = [];
+  symptomeIds: number[] = []; 
+  rdvs: any[] = [];
+  medecins: any[] = [];
+  listeSymptomes: any[] = [];
+  isLoading = false; 
+  today: Date = new Date();
 
-constructor(private auth: Auth, private router: Router, private user: User) {}
+  constructor(private auth: Auth, private router: Router, private user: User) {}
 
-ngOnInit(): void {
-  if (!this.auth.isAuth()) {
-    this.router.navigate(['/login']);
-    return;
+  ngOnInit(): void {
+    if (!this.auth.isAuth()) {
+      this.router.navigate(['/login']);
+      return;
+    }
+    this.loadInitialData();
   }
 
-  this.user.getAllrdv().subscribe((data: any) => {
-    this.rdvs = data;
-  });
-  this.user.getDisponibilite(this.auth.getId()).subscribe((data: any) => {
-    this.disponibilites = data;
-  });
-  this.user.getAllMdc().subscribe((data: any) => {
-    this.medecins = data;
+  
+  loadInitialData() {
+  this.isLoading = true;
+  forkJoin({
+    rdvs: this.user.getAllrdv(),
+    medecins: this.user.getAllMdc(),
+    symptomes: this.user.getAllSymptomes()
+  }).subscribe({
+    next: (res: any) => { 
+      this.rdvs = res.rdvs as any[]; 
+      this.medecins = res.medecins as any[];
+      
+      this.listeSymptomes = (res.symptomes as any[]).map((s: any) => ({ 
+        ...s, 
+        selected: false 
+      }));
+      
+      this.isLoading = false;
+    },
+    error: (err) => {
+      console.error("Erreur chargement:", err);
+      this.isLoading = false;
+    }
   });
 }
 
-createRdv() {
-    if (!this.nom_patient || !this.date_rdv || !this.heure_rdv || !this.medecinId) {
-      alert("Veuillez remplir tous les champs obligatoires !");
+  toggleSymptome(symptome: any) {
+    symptome.selected = !symptome.selected;
+    this.updateSymptomeIds();
+  }
+
+  private updateSymptomeIds() {
+    this.symptomeIds = this.listeSymptomes
+      .filter(s => s.selected)
+      .map(s => s.id);
+  }
+
+  createRdv() {
+    if (!this.form.nom_patient || !this.form.date_rdv || !this.form.heure_rdv || !this.form.medecinId || this.symptomeIds.length === 0) {
+      alert(" Formulaire incomplet !");
       return;
     }
 
-    const rdv = {
-      nom_patient: this.nom_patient,
-      email_patient: this.email_patient,
-      numero_patient: this.numero_patient,
-      symptome: this.symptome,
-      date_rdv: this.date_rdv,
-      heure_rdv: this.heure_rdv,
-      specialiteId: this.specialiteId ? parseInt(this.specialiteId) : null,
-      medecinId: parseInt(this.medecinId), 
+    const payload = {
+      ...this.form,
+      symptomeIds: this.symptomeIds,
+      medecinId: parseInt(this.form.medecinId)
     };
 
-    this.user.createRdv(rdv).subscribe({
+    this.user.createRdv(payload).subscribe({
       next: () => {
-        
-        this.user.getAllrdv().subscribe((data: any) => (this.rdvs = data));
-        this.nom_patient = '';
-        this.email_patient = '';
-        this.numero_patient = '';
-        this.symptome = '';
-        this.date_rdv = '';
-        this.heure_rdv = '';
-        this.specialiteId = '';
-        this.medecinId = '';
+        alert(" Rendez-vous enregistré");
+        this.resetForm();
+        this.refreshRdvs();
       },
-      error: (err) => console.error("Erreur création RDV:", err),
+      error: (err) => alert("Erreur : " + err.error?.message)
     });
   }
-dec(){
+
+  refreshRdvs() {
+  this.user.getAllrdv().subscribe((data: any) => {
+    this.rdvs = data as any[];
+  });
+}
+
+  resetForm() {
+    this.form = {
+      nom_patient: '',
+      email_patient: '',
+      numero_patient: '',
+      date_rdv: '',
+      heure_rdv: '',
+      medecinId: ''
+    };
+    this.listeSymptomes.forEach(s => s.selected = false);
+    this.symptomeIds = [];
+  }
+
+  dec() {
     this.auth.logoutUser();
     this.router.navigate(['/login']);
   }

33ad627e60b0ef45085c431a148c9c9da1277e5a
Mgreat01 <ephraimmonga5@gmail.com>
2026-01-07 17:08:05 +0100

    feat: asuppression d un comm

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ import { routes } from './app/app.routes';
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()), // ✔️ ici, PAS dans App
+    provideHttpClient(withInterceptorsFromDi()), 
   ]
 });
